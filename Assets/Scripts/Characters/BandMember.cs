068912a4d207e4030cd1cd040cd75ea6b336a111
Marshall Krueger <mgkrueger4@gmail.com>
2021-07-23 18:09:58 -0600

    More Band Member stuff

--- Assets/Scripts/Characters/BandMember.cs
+++ Assets/Scripts/Characters/BandMember.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class BandMember : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Texture icon;
+    public int maxHealth;
+    int currentHealth;
+    Stats stats;
+    BandMemberScriptableObject memberData;
+    MemberType type;
+
+    private void Start()
     {
-        
+        currentHealth = maxHealth;
+
+        stats.talent = 0;
+        stats.technical = 0;
+        stats.finesse = 0;
+        stats.hardiness = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddBonuses()
     {
-        
+        type = memberData.memberType;
+        if (memberData.statBoosts.talent)
+        {
+            stats.talent++;
+        }
+        if (memberData.statBoosts.technical)
+        {
+            stats.technical++;
+        }
+        if (memberData.statBoosts.finesse)
+        {
+            stats.finesse++;
+        }
+        if (memberData.statBoosts.hardiness)
+        {
+            stats.hardiness++;
+        }
     }
+
+
+}
+
+struct Stats
+{
+    public int talent;
+    public int technical;
+    public int finesse;
+    public int hardiness;
 }
---
+++ Assets/Scripts/Characters/BandMembers/BandMemberScriptableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BandMemberScriptableObject", order = 1)]
+public class BandMemberScriptableObject : ScriptableObject
+{
+
+    public MemberType memberType;
+    public StatBoosts statBoosts;
+    
+
+}
+
+public enum MemberType { Vocalist, Guitarist, Bassist, Drummer };
+
+[System.Serializable]
+public struct StatBoosts
+{
+    public bool talent;
+    public bool technical;
+    public bool finesse;
+    public bool hardiness;
+}
