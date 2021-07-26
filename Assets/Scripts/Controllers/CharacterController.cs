2ee64e3f098fba572e03b4a2d602523ba385a09b
Marshall Krueger <mgkrueger4@gmail.com>
2021-07-25 21:14:08 -0600

    Adding some code for animation

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -12,8 +12,11 @@ public class CharacterController : MonoBehaviour
     private Animator currentAnimator;
     private BandMember currentMember;
     private Collider2D currentCollider;
+    private SpriteRenderer currentRenderer;
 
     private bool interacting = false;
+    private bool horizontal = false;
+    private bool moving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,7 @@ public class CharacterController : MonoBehaviour
         currentAnimator = currentCharacter.GetComponent<Animator>();
         currentMember = currentCharacter.GetComponent<BandMember>();
         currentCollider = currentCharacter.GetComponent<Collider2D>();
+        currentRenderer = currentCharacter.GetComponent<SpriteRenderer>();
 
     }
 
@@ -49,6 +53,38 @@ public class CharacterController : MonoBehaviour
     {
         Movement();
         Assign();
+    }
+
+    private void UpdateAnimation(float forwardBack, float leftRight)
+    {
+        
+
+        if((Mathf.Abs(forwardBack) > 0) || (Mathf.Abs(leftRight) > 0)){
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+
+        if(leftRight < 0)
+        {
+            currentRenderer.flipX = true;
+            horizontal = true;
+        }
+        else if(leftRight > 0)
+        {
+            currentRenderer.flipX = false;
+            horizontal = true;
+        }
+        else if(moving)
+        {
+            horizontal = false;
+        }
+
+        currentAnimator.SetBool("moving", moving);
+
+        currentAnimator.SetBool("horizontal", horizontal);
     }
 
     private void Interact()
@@ -81,5 +117,7 @@ public class CharacterController : MonoBehaviour
         float speed = currentMember.GetSpeed();
 
         currentRigidbody2D.velocity = new Vector2(leftRight, forwardBack).normalized * speed;
+
+        UpdateAnimation(forwardBack, leftRight);
     }
 }
